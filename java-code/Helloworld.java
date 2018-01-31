a83c354cf6195a0d881b7e6384e9eb69a1e0badb
ec2-user <sree.aws17@gmail.com>
2018-01-31 08:08:15 +0000

    second commit

--- java-code/Helloworld.java
+++ java-code/Helloworld.java
@@ -1,6 +1,8 @@
+ 
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World");
+      System.out.println("Hello, Worldsecond  Mofied");
+      System.out.println("Hello, SECOND  Modifiction);
    }
-}
+      }
